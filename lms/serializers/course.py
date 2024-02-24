e230df87f2f2a75c44c1e16af9e5f0ba0d5cd47c
twist <twistysmig@mail.ru>
2024-02-24 15:16:00 +0300

    24.2 dz

--- lms/serializers/course.py
+++ lms/serializers/course.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from lms.models import Course
+from lms.serializers.lesson import LessonSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    def get_lesson_count(self, obj):
+        return obj.lesson_set.count()
+
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'lesson_count', 'lessons']
+
--- lms/serializers/lesson.py
+++ lms/serializers/lesson.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
-from lms.models import Lesson
+from lms.models import Course, Lesson
 
 
 class LessonSerializer(serializers.ModelSerializer):
+    course = serializers.PrimaryKeyRelatedField(queryset=Course.objects.all())
     class Meta:
         model = Lesson
         fields = '__all__'
