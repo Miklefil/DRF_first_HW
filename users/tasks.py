ad9e9a512b2c555dcba819521612cf1fd54e5c0d
twist <twistysmig@mail.ru>
2024-03-31 17:08:50 +0300

    26.2 корректировки на периодическую проверку активности пользователей с последующей блокой

--- users/tasks.py
+++ users/tasks.py
@@ -1,14 +1,16 @@
 from celery import shared_task
 from users.models import User
 from datetime import datetime, timedelta
+from django.conf import settings
+import pytz
 
 
 @shared_task
 def check_all_users_last_login():
-    date_x = datetime.now() - timedelta(days=30)
-    users = User.objects.all()
-    for user in users:
-        if user.last_login < date_x:
-            user.is_active = False
-            user.save()
-            print(f"Пользователь{user} был заблокирован.")
+    zone = pytz.timezone(settings.TIME_ZONE)
+    date_x = datetime.now(zone) - timedelta(days=30)
+    users_to_deactivate = User.objects.filter(last_login__lt=date_x)
+    for user in users_to_deactivate:
+        user.is_active = False
+        user.save()
+        print(f"{user} был заблокирован")
